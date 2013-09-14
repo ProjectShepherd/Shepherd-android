4f8e23dba822065ba9732f17f5ef7f9074e92870
Gautam Korlam <gautam.korlam@lookout.com>
2013-09-14 10:45:27 -0700

    Added person fields and json parse with fuzzy search

--- src/com/shepherd/api/Person.java
+++ src/com/shepherd/api/Person.java
@@ -1,5 +1,16 @@
 package com.shepherd.api;
 
 public class Person {
-
+    public String firstName;
+    public String middleName;
+    public String lastName;
+    public int age;
+    public int height;
+    public int weight;
+    public String sex;
+    public String eye;
+    public String hair;
+    public String race;
+    public String description;
+    public String photo;
 }
