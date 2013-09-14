67211bf159576d0e5b9668f76d15e8df97cc4ca0
unknown <yijiewang@gatech.edu>
2013-09-14 02:30:39 -0400

    Basic version of missing people detail.

--- src/com/shepherd/MissingPersonDetail.java
+++ src/com/shepherd/MissingPersonDetail.java
@@ -1,12 +1,19 @@
 package com.shepherd;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.TextView;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass. Activities that
@@ -16,34 +23,33 @@ import android.view.ViewGroup;
  * method to create an instance of this fragment.
  * 
  */
-public class MissingPersonDetail extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+public class MissingPersonDetail extends Fragment implements OnClickListener {
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
-
-	private OnFragmentInteractionListener mListener;
+	private static final String PERSON_DETAIL_FIRST_NAME = "first_name";
+	private static final String PERSON_DETAIL_MIDDLE_NAME = "middle_name";
+	private static final String PERSON_DETAIL_LAST_NAME = "last_name";
 
 	/**
 	 * Use this factory method to create a new instance of this fragment using
 	 * the provided parameters.
 	 * 
-	 * @param param1
-	 *            Parameter 1.
-	 * @param param2
-	 *            Parameter 2.
+	 * @param personDetail
+	 *            the detail info to be displayed.
 	 * @return A new instance of fragment MissingPersonDetail.
 	 */
-	// TODO: Rename and change types and number of parameters
-	public static MissingPersonDetail newInstance(String param1, String param2) {
+	public static MissingPersonDetail newInstance(JSONObject personDetail) {
 		MissingPersonDetail fragment = new MissingPersonDetail();
 		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
+		try {
+			args.putString(PERSON_DETAIL_FIRST_NAME,
+					personDetail.getString(PERSON_DETAIL_FIRST_NAME));
+			args.putString(PERSON_DETAIL_MIDDLE_NAME,
+					personDetail.getString(PERSON_DETAIL_MIDDLE_NAME));
+			args.putString(PERSON_DETAIL_LAST_NAME,
+					personDetail.getString(PERSON_DETAIL_LAST_NAME));
+		} catch (JSONException e) {
+			throw new IllegalArgumentException(e);
+		}
 		fragment.setArguments(args);
 		return fragment;
 	}
@@ -55,56 +61,41 @@ public class MissingPersonDetail extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+		
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_missing_person_detail,
+		View view = inflater.inflate(R.layout.fragment_missing_person_detail,
 				container, false);
-	}
-
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
+		if (getArguments() != null) {
+			((TextView)(view.findViewById(R.id.personName))).setText(
+					String.format("%s %s %s",
+							getArguments().getString(PERSON_DETAIL_FIRST_NAME),
+							getArguments().getString(PERSON_DETAIL_MIDDLE_NAME),
+							getArguments().getString(PERSON_DETAIL_LAST_NAME)
+							));
 		}
+		view.findViewById(R.id.markFound).setOnClickListener(this);
+		return view;
 	}
 
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		try {
-			mListener = (OnFragmentInteractionListener) activity;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
 	}
 
 	@Override
 	public void onDetach() {
 		super.onDetach();
-		mListener = null;
 	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated to
-	 * the activity and potentially other fragments contained in that activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		public void onFragmentInteraction(Uri uri);
+	// Mark Found.
+	@Override
+	public void onClick(View v) {
+		getActivity().onBackPressed();
 	}
 
 }
