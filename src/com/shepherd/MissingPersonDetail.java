6d1715d01e95009cea884a9e04eeb836e61ac65f
ekux44 <ekux44@gmail.com>
2013-09-14 02:41:00 -0500

    project fixes

--- src/com/shepherd/MissingPersonDetail.java
+++ src/com/shepherd/MissingPersonDetail.java
@@ -4,15 +4,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
-import android.view.KeyEvent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
 import android.widget.TextView;
 
 /**
